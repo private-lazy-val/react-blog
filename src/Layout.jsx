0a997ff32c20292fd862c351f386d9692f7cd3c0
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-09-12 16:08:08 +0400

    update title

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ import {Outlet} from 'react-router-dom';
 const Layout = () => {
     return (
         <div className='app'>
-            <Header title='Pekingese Corner &#128062; &#128054;'/>
+            <Header title='Pekingese Corner &#128054;'/>
             <Nav/>
             <Outlet/>
             <Footer/>
