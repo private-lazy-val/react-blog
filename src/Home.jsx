36987a205c14fe5f7369e0c9d3650ebd4371921d
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-09-22 13:45:30 +0400

    add store

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,6 @@
 import Feed from './Feed';
 import {useContext} from "react";
 import DataContext from "./context/DataContext";
-
 const Home = () => {
     const {searchResults, fetchError, isLoading} = useContext(DataContext);
 
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,6 @@ import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import {Outlet} from 'react-router-dom';
-
 const Layout = () => {
     return (
         <div className='app'>
