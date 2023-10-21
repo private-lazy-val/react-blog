7eeb2748c19f239c732fa412a90c37de060354b6
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-10-21 14:43:45 +0400

    update server.js

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const router = jsonServer.router('./data/db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+const html = `
+    <h1>Welcome to Pekingese Corner API</h1>
+    <p>Available routes:</p>
+    <ul>
+        <li><a href="https://pekingese-blog-8ceaec0c8c78.herokuapp.com/posts">/posts</a></li>
+    </ul>
+`;
 // Custom route for the homepage
 server.get('/', (req, res) => {
-    res.send('<h1>Welcome to Pekingese Corner API</h1><p>Available routes: <a href="https://pekingese-blog-8ceaec0c8c78.herokuapp.com/posts">/posts</a></p>');
+    res.send(html);
 });
 
 // Use middlewares and router
